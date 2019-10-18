68d9f05b81a1e76a3fb36e950ee13cf27bed7b31
kazimurtuza <kazimurtuza11@gmail.com>
2019-10-18 16:11:50 +0600

    collect data from table with bind associate prepared

--- form_pdo_prepare.php
+++ form_pdo_prepare.php
@@ -15,46 +15,23 @@
       echo "NO CONN". $e->getMessage();
   }
 ?>
-<!-- data collect from bd  with out  bind prepared-->
-<?php
-
-// try{
-//   $sql="SELECT * FROM form_tb";
-//   $result= $conn->prepare($sql);
-//   $result->execute();
- 
-//   while( $row=$result->fetch(PDO::FETCH_ASSOC))
-//   {
-//   echo "id =".$row['id']."<br/>";
-//   echo "name =".$row['name']."<br/>";
-//   echo "country =".$row['country']."<br/>";
-//   echo "sallary =".$row['sallary']."<br/>";
-//   echo"<br/> <br/>";
-//   }
-
-// }
-// catch(PDOException $w)
-// {
-//   echo $w->getMessage();
-// }
-
-?>
 
 
-<!-- ----------------collect data from table with bind prepared ----------------->
+
+<!-- ----------------collect data from table with bind associate prepared ----------------->
 <?php 
 try{
 $qui="SELECT * FROM form_tb";
 $result=$conn->prepare($qui);
 $result->execute();
 
-//////////bind by column number///////////////////
+//////////bind by column ASSOCIATE//////////////////
 
-$result->bindColumn(1,$id);
-$result->bindColumn(2,$name);
-$result->bindColumn(3,$phon);
-$result->bindColumn(4,$country);
-$result->bindColumn(5,$sallary);
+$result->bindColumn('id',$id);
+$result->bindColumn('name',$name);
+$result->bindColumn('phon',$phon);
+$result->bindColumn('country',$country);
+$result->bindColumn('sallary',$sallary);
 
 while($result->fetch(PDO::FETCH_ASSOC))
 {
