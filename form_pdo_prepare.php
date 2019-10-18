3dd00508ce439f401d780f0df2d6c93d6981db02
kazimurtuza <kazimurtuza11@gmail.com>
2019-10-18 16:00:41 +0600

    collect data from table with bind prepared

--- form_pdo_prepare.php
+++ form_pdo_prepare.php
@@ -15,27 +15,59 @@
       echo "NO CONN". $e->getMessage();
   }
 ?>
-<!-- data collect from bd -->
+<!-- data collect from bd  with out  bind prepared-->
 <?php
 
-try{
-  $sql="SELECT * FROM form_tb";
-  $result= $conn->prepare($sql);
-  $result->execute();
+// try{
+//   $sql="SELECT * FROM form_tb";
+//   $result= $conn->prepare($sql);
+//   $result->execute();
  
-  while( $row=$result->fetch(PDO::FETCH_ASSOC))
-  {
-  echo "id =".$row['id']."<br/>";
-  echo "name =".$row['name']."<br/>";
-  echo "country =".$row['country']."<br/>";
-  echo "sallary =".$row['sallary']."<br/>";
-  echo"<br/> <br/>";
-  }
+//   while( $row=$result->fetch(PDO::FETCH_ASSOC))
+//   {
+//   echo "id =".$row['id']."<br/>";
+//   echo "name =".$row['name']."<br/>";
+//   echo "country =".$row['country']."<br/>";
+//   echo "sallary =".$row['sallary']."<br/>";
+//   echo"<br/> <br/>";
+//   }
+
+// }
+// catch(PDOException $w)
+// {
+//   echo $w->getMessage();
+// }
+
+?>
+
+
+<!-- ----------------collect data from table with bind prepared ----------------->
+<?php 
+try{
+$qui="SELECT * FROM form_tb";
+$result=$conn->prepare($qui);
+$result->execute();
+
+//////////bind by column number///////////////////
+
+$result->bindColumn(1,$id);
+$result->bindColumn(2,$name);
+$result->bindColumn(3,$phon);
+$result->bindColumn(4,$country);
+$result->bindColumn(5,$sallary);
+
+while($result->fetch(PDO::FETCH_ASSOC))
+{
+  echo " id :".$id.", name :".$name.", phon :".$phon.", country :".$country.", sallary  : ".$sallary."<br/><br/>";
+}
+
 
 }
-catch(PDOException $w)
+catch(PDOException $e)
 {
-  echo $w->getMessage();
+  echo $e->getMessage();
 }
+
+
 
 ?>
