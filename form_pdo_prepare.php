b5e7f31066927d50385db1f3309f9ad46d5121a7
kazimurtuza <kazimurtuza11@gmail.com>
2019-10-18 15:33:23 +0600

    collect data  from table  with out use bind repared

--- form_pdo_prepare.php
+++ form_pdo_prepare.php
@@ -15,3 +15,27 @@
       echo "NO CONN". $e->getMessage();
   }
 ?>
+<!-- data collect from bd -->
+<?php
+
+try{
+  $sql="SELECT * FROM form_tb";
+  $result= $conn->prepare($sql);
+  $result->execute();
+ 
+  while( $row=$result->fetch(PDO::FETCH_ASSOC))
+  {
+  echo "id =".$row['id']."<br/>";
+  echo "name =".$row['name']."<br/>";
+  echo "country =".$row['country']."<br/>";
+  echo "sallary =".$row['sallary']."<br/>";
+  echo"<br/> <br/>";
+  }
+
+}
+catch(PDOException $w)
+{
+  echo $w->getMessage();
+}
+
+?>
